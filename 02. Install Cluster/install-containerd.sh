0bff7f598da83061f4dc3d59054da631cb5ed3bc
Nana Janashia <nn_janashia@yahoo.com>
2022-09-23 10:42:16 +0000

    Update install-containerd.sh

--- 02. Install Cluster/install-containerd.sh
+++ 02. Install Cluster/install-containerd.sh
@@ -2,7 +2,7 @@
 
 # Install and configure prerequisites
 ## load the necessary modules for Containerd
-cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
 EOF
@@ -10,14 +10,14 @@ EOF
 sudo modprobe overlay
 sudo modprobe br_netfilter
 
-## setup the required kernel/sysctl parameters
-cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
-net.bridge.bridge-nf-call-iptables = 1
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
-net.ipv4.ip_forward = 1
+net.ipv4.ip_forward                 = 1
 EOF
 
-## Apply sysctl params without reboot
+# Apply sysctl params without reboot
 sudo sysctl --system
 
 # Install containerd
@@ -27,4 +27,5 @@ sudo apt-get -y install containerd
 # Configure containerd with defaults and restart with this config
 sudo mkdir -p /etc/containerd
 containerd config default | sudo tee /etc/containerd/config.toml
+sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
 sudo systemctl restart containerd
